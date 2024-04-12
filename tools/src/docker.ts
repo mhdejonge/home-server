6ae4efbcb7e3dc1957a27fd4f5c97888cfa622ae
Matthew DeJonge <mhdejong@umich.edu>
2024-04-12 18:38:01 -0400

    docker error message change

--- tools/src/docker.ts
+++ tools/src/docker.ts
@@ -8,7 +8,7 @@ export function docker(args: string[], cwd?: string): void {
   }
   spawn('docker', args, { cwd, stdio: 'inherit' }).on('close', code => {
     if (code) {
-      console.error(`${args.join(' ')} exited with code ${code}.`);
+      console.error(`docker ${args.join(' ')} exited with code ${code}.`);
     }
   });
 }
